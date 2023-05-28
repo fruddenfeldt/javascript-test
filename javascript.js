4a3c1e17b69cb889f1d20f26c2b22cacd7037683
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-28 21:57:36 +0200

    exercise complete

--- javascript.js
+++ javascript.js
@@ -1,6 +1,50 @@
-const para = document.createElement('p')
-const div = document.createElement('div')
+/* 
+- a <p> with red text that says “Hey I’m red!”
+- an <h3> with blue text that says “I’m a blue h3!”
+- a <div> with a black border and pink background color with the following elements inside of it:
+- another <h1> that says “I’m in a div”
+- a <p> that says “ME TOO!”
+*/ 
 
-  para.classList.add('paraCSS');
-  para.textContent = 'I am red!';
-  container.appendChild(para);
+const paraOne = document.createElement('p')
+
+// Red <p> element:
+  paraOne.classList.add('paraCSS');
+  paraOne.textContent = 'I am red!';
+  container.appendChild(paraOne);
+  paraOne.style.color = 'red';
+  paraOne.style.fontSize = '16px';
+
+
+// blue <h3> element:
+
+const h3 = document.createElement('h3')
+h3.classList.add('h3style');
+h3.textContent = 'I am a blue h3';
+container.appendChild(h3);
+h3.style.color = 'blue';
+
+// <div> with a black border and pink background color:
+
+const div = document.createElement('div')
+div.classList.add = 'divStyle'
+container.appendChild(div)
+Object.assign(div.style, {
+  border: "solid black 2px",
+  backgroundColor: "pink",
+  // padding: "50px"
+})
+
+// another <h1> that says “I’m in a div”
+const h1 = document.createElement('h1')
+h1.classList.add('h1Style')
+h1.textContent = ('I am in a div')
+div.appendChild(h1)
+
+// another <h1> that says “I’m in a div”
+const paraTwo = document.createElement('p')
+// paraTwo.classList.add('')
+paraTwo.textContent = ('ME TOO!')
+div.appendChild(paraTwo)
+
+
